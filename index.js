dd8e8a2ba834282e111dbd1a3194a945689d4866
Artur Delura <artur.delura@capita.co.uk>
2016-06-09 21:07:40 +0100

    More promises.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 
 var q = require("q");
-var path = require('path');
 var inquirer = require("inquirer");
 var program = require('commander');
 var exec = require('child-process-promise').exec;
 var utils = require('./lib/utils');
-var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
 program.version('0.1.0');
 
@@ -14,60 +12,60 @@ program
     .command('list')
     .description('Show all globally linked packages')
     .action(function () {
-        var linkedModules = utils.findSymLinks('./node_modules');
-
-        console.log('Currently linked modules to this one\n');
-        console.log(linkedModules.join('\n'));
+        utils.findSymLinks('./node_modules').then(function (linkedModules) {
+            console.log('Currently linked packages to this one\n');
+            console.log(linkedModules.join('\n'));
+        });
     });
 
 program
     .command('link')
-    .description('Link chosen globally available modules')
+    .description('Link chosen globally available packages')
     .action(function () {
-        var globalLinkedModules = utils.findSymLinks(path.join(home, '/AppData/Roaming/npm/node_modules'));
+        utils.getGloballyLinkedPackages().then(function (globalLinkedModules) {
+            inquirer.prompt({
+                message: 'Which repository you want to link?',
+                type: 'checkbox',
+                name: 'packages',
+                choices: globalLinkedModules
+            }, function (answer) {
+                if (answer.packages.length == 0) {
+                    return;
+                }
 
-        inquirer.prompt({
-            message: 'Which repository you want to link?',
-            type: 'checkbox',
-            name: 'modules',
-            choices: globalLinkedModules
-        }, function (answer) {
-            if (answer.modules.length == 0) {
-                return;
-            }
-
-            q.all(answer.modules.map(function (module) {
-                return exec('npm link ' + module);
-            })).then(function () {
-                console.log('done');
-            }).catch(function (err) {
-                console.log('err', err);
+                q.all(answer.packages.map(function (module) {
+                    return exec('npm link ' + module);
+                })).then(function () {
+                    console.log('Successfully linked all packages');
+                }).catch(function (err) {
+                    console.log('Error', err);
+                });
             });
         });
     });
 
 program
     .command('unlink')
-    .description('Unlink already linked modules')
+    .description('Unlink chosen from already linked packages')
     .action(function () {
-        var linkedModules = utils.findSymLinks('./node_modules');
+        utils.getLocallyLinkedPackages().then(function (linkedPackages) {
+            inquirer.prompt({
+                message: 'Which repository you want to unlink?',
+                type: 'checkbox',
+                name: 'packages',
+                choices: linkedPackages
+            }, function (answer) {
+                if (answer.packages.length == 0) {
+                    return;
+                }
 
-        inquirer.prompt({
-            message: 'Which repository you want to unlink?',
-            type: 'checkbox',
-            name: 'modules',
-            choices: linkedModules
-        }, function (answer) {
-            if (answer.modules.length == 0) {
-                return;
-            }
-
-            q.all(answer.modules.map(function (module) {
-                return exec('npm unlink ' + module);
-            })).then(function () {
-                console.log('done');
-            }).catch(function (err) {
-                console.log('err', err);
+                q.all(answer.packages.map(function (module) {
+                    return exec('npm unlink ' + module);
+                })).then(function () {
+                    console.log('done');
+                }).catch(function (err) {
+                    console.log('Error', err);
+                });
             });
         });
     });
